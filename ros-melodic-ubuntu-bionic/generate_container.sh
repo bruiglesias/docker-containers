14ab3f5cdcf918073efc9c300f7f6a187c5d050b
Bruno Iglesias <bruno.iglesias.eng@gmail.com>
2021-08-26 17:08:24 -0400

    Mudanças no volume

--- ros-melodic-ubuntu-bionic/generate_container.sh
+++ ros-melodic-ubuntu-bionic/generate_container.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-mkdir -p src
+mkdir -p ros_workspace
 
 # create a shared volume to store the ros_ws
 docker volume create --driver local \
     --opt type="none" \
-    --opt device="${PWD}/src/" \
+    --opt device="${PWD}/ros_workspace/" \
     --opt o="bind" \
-    "ros-melodic _src_vol"
+    "ros-melodic_src_vol"
  
 xhost +
 docker run \
     --net=host \
     --env="DISPLAY" \
     -it \
-    --volume="ros-melodic_src_vol:/home/ros/ros_workspace/src/:rw" \
+    --volume="ros-melodic_src_vol:/home/ros/ros_workspace/:rw" \
     --name=ros-melodic \
     --device=/dev/dri:/dev/dri \
     bruiglesias/ros-melodic-ubuntu-bionic:latest
